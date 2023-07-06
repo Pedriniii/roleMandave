fbd658adf3278d00a2621dab6610b3e16b258246
Pedrini <felipedrinidosantos@outlook.com>
2023-07-06 10:25:51 -0300

    detalhamento dos passeios

--- src/Controllers/Passeios/read.js
+++ src/Controllers/Passeios/read.js
@@ -13,4 +13,21 @@ const listarPasseios = async (req, res) => {
     
 };
 
-module.exports = listarPasseios;
+const detalharPasseio = async (req, res) => {
+    const { id } = req.body
+    console.log(id)
+    try{
+        const detalhePasseio  = await conn('passeios')
+        .join('pessoas_passeios','passeios.id', '=', 'pessoas_passeios.id_passeio')
+        .join('pessoas', 'pessoas_passeios.id_pessoa', '=', 'pessoas.id')
+        .select('passeios.id', 'passeios.descricao', 'pessoas.id', 'pessoas.nome')
+        .where('passeios.id', '=', id)
+
+        return res.json({detalhePasseio})
+    }catch(error) {
+     console.log(error)
+     res.status(500).json({erro: error})   
+    }
+    
+};
+module.exports = { listarPasseios, detalharPasseio };
--- src/Routers/passeios.js
+++ src/Routers/passeios.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cadastrarPasseio = require('../Controllers/Passeios/create')
-const listarPasseios  = require('../Controllers/Passeios/read');
+const {listarPasseios, detalharPasseio}  = require('../Controllers/Passeios/read');
 const atualizarPasseio = require('../Controllers/Passeios/update');
 const verificarLogin = require('../Middlewares/authLogin');
 
@@ -8,6 +8,7 @@ const rotasPasseios = express();
 
 // rotas.post('/cadastrarPasseios', cadastrarPasseios);
 rotasPasseios.get('/listarPasseios',verificarLogin , listarPasseios);
+rotasPasseios.get('/detalharPasseio',verificarLogin , detalharPasseio);
 rotasPasseios.post('/cadastrarPasseio',verificarLogin , cadastrarPasseio);
 rotasPasseios.put('/atualizarPasseio',verificarLogin , atualizarPasseio);
 

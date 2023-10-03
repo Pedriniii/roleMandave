523bf58abaa23e5decd0721f1044f575724cf606
Pedrini <45887466+Pedriniii@users.noreply.github.com>
2023-10-03 17:29:57 -0300

    tela do orçamento individual
    a

--- src/Controllers/Orcamento/read.js
+++ src/Controllers/Orcamento/read.js
@@ -13,6 +13,22 @@ const listarOrcamentos = async (req, res) => {
     
 };
 
+const readOrcamento = async (req, res) => {
+    const { id } = req.body;
+
+    try{
+        const readOrcamento  = await conn.select('*')
+        .from('orcamentos')
+        .where('id','=', id)
+
+        return res.json({readOrcamento})
+    }catch(error) {
+     console.log(error)
+     res.status(500).json({error: 'Deu erro, porra'})   
+    }
+    
+};
+
 const orcamentoTotal = async(req, res)  => {
     try{
         const orcamentoTotal  = await conn('orcamentos')
@@ -25,4 +41,4 @@ const orcamentoTotal = async(req, res)  => {
 }
 
 
-module.exports = {listarOrcamentos,orcamentoTotal }
+module.exports = {listarOrcamentos, orcamentoTotal, readOrcamento }
--- src/Routers/orcamentos.js
+++ src/Routers/orcamentos.js
@@ -3,13 +3,14 @@ const cadastrarOrcamento = require('../Controllers/Orcamento/create');
 const updateOrcamento = require('../Controllers/Orcamento/put')
 // middleware
 const verificarLogin = require('../Middlewares/authLogin');
-const {listarOrcamentos, orcamentoTotal} = require('../Controllers/Orcamento/read');
+const {listarOrcamentos, orcamentoTotal, readOrcamento } = require('../Controllers/Orcamento/read');
 
 const rotasOrcamentos = express();
 
 
 rotasOrcamentos.post('/cadastrarOrcamento', cadastrarOrcamento);
 rotasOrcamentos.get('/listarOrcamento', listarOrcamentos);
+rotasOrcamentos.get('/readOrcamento/:id', readOrcamento);
 rotasOrcamentos.get('/orcamentoTotal', orcamentoTotal);
 rotasOrcamentos.put('/updateOrcamento/:id', updateOrcamento);
 
